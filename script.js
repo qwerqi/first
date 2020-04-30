4749c7f0ad34cb8cbac64fd8b74b3bfb1e292e02
Qwerq <a89605356416@gamil.com>
2020-04-30 16:30:56 +0300

    Это первый комит

---
+++ script.js
@@ -0,0 +1,75 @@
+'use strict';
+//console.log(4/0);
+// console.log('string'*9);
+// let something
+// console.log(something);
+// var obj ={};
+// let persone = {
+//     name: 'Jhon',
+//     age: '25',
+//     isMarrid: false
+// };
+
+// console.log(persone["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.png'];
+// console.log(arr[0]);
+// let a;
+// console.log(a);
+ 
+// const obj = {
+//     name: 'Alex'
+// };
+
+// const arr = [1, 2, 3, 4];
+// console.log(arr[0]);
+
+// // -to string
+// console.log(typeof(String(4)));
+// console.log(typeof('ww'+'5'));
+
+// // -to namber
+
+// console.log(typeof(Number('4')));
+
+// console.log(typeof(5 + +'5'));
+
+// console.log(typeof(parseInt('15px', 10)));
+
+// let ans = +prompt('hellow,', '');
+
+// // 0, '', null, undefined, NaN
+
+// let switcher  = null;
+
+// if (switcher) {
+//     console.log("Workicng..")
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//     console.log("Workicng..");
+// }
+
+// console.log(typeof(Boolean(4)));
+
+// alert('Hellow world');
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let answer = prompt("Есмть ли Вам 18?", 'Да');
+// console.log(typeof(answer));
+// let incr = 10;
+//     decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+// console.log(5%2);
+// console.log('2'===2);
+
+let isChecked = true;
+    isClose = false;
+
+console.log(!isChecked||isClose);
